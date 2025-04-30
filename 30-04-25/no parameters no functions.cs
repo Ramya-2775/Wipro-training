30d03bb36342807b5d9d54a875d8450cb290508f
Ramya Ane <ramyarams2775@gmail.com>
2025-04-30 12:57:35 +0530

    no parameters

---
+++ 30-04-25/no parameters no functions.cs
@@ -0,0 +1,15 @@
+
+using System;
+
+class program
+{
+    static void printmessage()
+    {
+        Console.WriteLine("Welcome");
+    }
+    static void Main()
+    {
+        printmessage();
+    }
+}
+    
