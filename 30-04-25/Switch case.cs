22e1087b5f37a2fdff762dde53ec292640fb3224
Ramya Ane <ramyarams2775@gmail.com>
2025-04-30 09:18:53 +0530

    switch case

---
+++ 30-04-25/Switch case.cs
@@ -0,0 +1,31 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int day =5;
+    switch (day)
+    {
+        case 1: Console.WriteLine("Monday");
+        break;
+        case 2:Console.WriteLine("Tuesday");
+        break;
+        case 3:Console.WriteLine("Wednesday");
+        break;
+        case 4:Console.WriteLine("Thursday");
+        break;
+        case 5:Console.WriteLine("Friday");
+        break;
+        case 6:Console.WriteLine("Saturday");
+        break;
+        default:
+        Console.WriteLine("Invalid day");
+        break;
+    }
+    }
+}
+        
