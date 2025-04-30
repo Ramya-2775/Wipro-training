fa3d4085795afdae55bd0e6898de9d851da4edc4
Ramya Ane <ramyarams2775@gmail.com>
2025-04-30 12:52:21 +0530

    greetuser

---
+++ 30-04-25/greetuser.cs
@@ -0,0 +1,14 @@
+using System;
+
+class program
+{
+    static void greetuser(string name)
+    {
+        Console.WriteLine("hello"  + name);
+    }
+    static void Main()
+    {
+        greetuser("Ramya");
+    }
+}
+    
