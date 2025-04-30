a8cfad3c21ca9f26efc6dd32fdd225e9f1ed862f
Ramya Ane <ramyarams2775@gmail.com>
2025-04-30 10:04:06 +0530

    exercise 1

---
+++ 30-04-25/Assignments/Exercise 1.cs
@@ -0,0 +1,55 @@
+//Given a month number(1-12) print its name
+
+using System;
+
+public class MonthName
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter month number (1-12): ");
+        int month = Convert.ToInt32(Console.ReadLine());
+
+        switch (month)
+        {
+            case 1:
+                Console.WriteLine("January");
+                break;
+            case 2:
+                Console.WriteLine("February");
+                break;
+            case 3:
+                Console.WriteLine("March");
+                break;
+            case 4:
+                Console.WriteLine("April");
+                break;
+            case 5:
+                Console.WriteLine("May");
+                break;
+            case 6:
+                Console.WriteLine("June");
+                break;
+            case 7:
+                Console.WriteLine("July");
+                break;
+            case 8:
+                Console.WriteLine("August");
+                break;
+            case 9:
+                Console.WriteLine("September");
+                break;
+            case 10:
+                Console.WriteLine("October");
+                break;
+            case 11:
+                Console.WriteLine("November");
+                break;
+            case 12:
+                Console.WriteLine("December");
+                break;
+            default:
+                Console.WriteLine("Invalid month number!");
+                break;
+        }
+    }
+}
