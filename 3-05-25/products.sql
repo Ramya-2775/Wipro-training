d80c67991c6afeb70c573eb03a5beab6e83d7409
Ramya <ramyarams2775@gmail.com>
2025-05-03 17:16:53 +0530

    products

--- 3-05-25/products.sql
+++ 3-05-25/products.sql
@@ -11,3 +11,53 @@ create table orders(
 	orderdate date,
 	foreign key (productID) references products(productID)
 );
+
+insert into products (productID, productname, price)
+values 
+(1, 'Laptop', 70000.00),
+(2, 'Smartphone', 30000.00),
+(3, 'Tablet', 20000.00),
+(4, 'Monitor', 15000.00);
+
+insert into orders (orderID, productID, quantity, orderdate)
+values
+(101, 1, 2, '2024-12-01'),
+(102, 2, 1, '2024-12-05'),
+(103, 3, 3, '2024-12-10'),
+(104, 1, 1, '2024-12-15'),
+(105, 4, 2, '2024-12-20');
+
+select 
+	orders.orderID,
+	products.productname,
+	orders.quantity,
+	products.price,
+	(orders.quantity * products.price) as TotalAmount,
+	orders.orderdate
+from orders
+inner join products on orders.productID = products.productID;
+
+select 
+	products.productID,
+	products.productname,
+	orders.orderID,
+	orders.quantity,
+	orders.orderdate
+from products
+left join orders on products.productID = orders.productID;
+
+select 
+	products.productID,
+	products.productname,
+	orders.orderID,
+	orders.quantity,
+	orders.orderdate
+from products
+right join orders on products.productID = orders.productID;
+
+select
+	products.productname,
+	sum(orders.quantity) as totalunitsold
+from orders
+inner join products on orders.productID = products.productID
+group by products.productname;
