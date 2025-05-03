1f7fcc154592be44fd955d05bd2b9ed7c0fe936f
Ramya <ramyarams2775@gmail.com>
2025-05-03 13:01:40 +0530

    insert

---
+++ 3-05-25/insert.sql
@@ -0,0 +1,4 @@
+insert into employees(employeeID, Firstname, Lastname, Department, Salary, Hiredata)
+values
+(1, 'Ramya', 'Ane', 'IT', 50000.00, '2025-06-03'),
+(2, 'prasad', 'Reddy', 'HR', 70000.9, '2025-09-04');
