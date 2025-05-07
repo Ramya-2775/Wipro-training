14e86fe64d328df86c2794efea000aa23f1e7257
Ramya <ramyarams2775@gmail.com>
2025-05-07 12:30:29 +0530

    remove

---
+++ 07-05-25/Assignment 1/removeelement.cs
@@ -0,0 +1,22 @@
+
+//Arraylist insert element
+using System;
+using System.Collections;
+class program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        list.Add("Apple");
+        list.Add(42);
+        list.Add(3.14);
+        list.Remove(42);
+        list.RemoveAt(0);
+        Console.WriteLine("count after removal " + list.Count);
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.Read();
+    }
+}
