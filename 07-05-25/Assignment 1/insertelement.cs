cc785d4e9626c147d9e680143baa33d6c7c52cda
Ramya <ramyarams2775@gmail.com>
2025-05-07 12:14:06 +0530

    insert

---
+++ 07-05-25/Assignment 1/insertelement.cs
@@ -0,0 +1,23 @@
+
+
+//Arraylist insert element
+using System;
+using System.Collections;
+class program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        list.Add("Apple");
+        list.Add(42);
+        list.Add(3.14);
+
+        list.Insert(1, "Inserted");
+        Console.WriteLine("Element at index 1: " + list[1]);
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.Read();
+    }
+}
