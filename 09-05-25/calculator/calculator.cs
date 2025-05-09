981e62eaaa5e8333120fbc73b626a876d2728835
Ramya <ramyarams2775@gmail.com>
2025-05-09 14:37:50 +0530

    calc

---
+++ 09-05-25/calculator/calculator.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double add(double a, double b)
+        {
+            return a + b;
+        }
+        public int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
