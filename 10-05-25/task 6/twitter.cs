30c9be035723fdd14b3a6dd37ea6a7e166e7f82c
Ramya <ramyarams2775@gmail.com>
2025-05-10 11:11:49 +0530

    twitter

---
+++ 10-05-25/task 6/twitter.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class twitter:socialmedia
+    {
+        public override void post()
+        {
+            Console.WriteLine("tweeting on twitter");
+        }
+    }
+}
