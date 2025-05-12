ced2896636e5488a3596e47997ed467bfc2bfd37
Ramya <ramyarams2775@gmail.com>
2025-05-12 16:10:08 +0530

    linq

---
+++ 12-05-25/linq/linq.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Ramya", "sowmya", "Anil", "reddy", "prasad" };
+        UsingLinq(names);
+
+    }
+    private static void UsingLinq(string[] names)
+    {
+        IEnumerable<string> query = from s in names
+                                    where s.Length == 5
+                                    orderby s
+                                    select s.ToUpper();
+        foreach (string item in query)
+        
+            Console.WriteLine(item);
+
+            Console.Read();
+        
+
+
+    }
+}
