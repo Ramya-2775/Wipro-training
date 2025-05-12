5562d1973ed2bfd4f07c9dcde0626393042ea178
Ramya <ramyarams2775@gmail.com>
2025-05-12 17:36:34 +0530

    type3

---
+++ 12-05-25/linq/linq func3.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        string[] names = { "Ramya", "sowmya", "Anil", "reddy", "prasad" };
+        UsingAnonymousMethods(names);
+
+    }
+    /*private static void UsingLinq(string[] names)
+    {
+        IEnumerable<string> query = from s in names
+                                    where s.Length == 5
+                                    orderby s
+                                    select s.ToUpper();
+        foreach (string item in query)
+
+            Console.WriteLine(item);
+
+        Console.Read();
+
+
+
+    }
+
+    private static void UsingLinqextensions(string[] names)
+    { 
+
+    IEnumerable<string> query = names
+                                .Where(s => s.Length == 5)
+                                .OrderBy(s => s)
+                                .Select(s => s.ToUpper());
+    foreach (string item in query)
+
+        Console.WriteLine(item);
+
+    Console.Read();
+
+
+
+    }
+
+    private static void UsingLinqextensions(string[] names)
+    {
+        Func<string, bool> filter = s => s.Length == 5;
+        Func<string, string> extract = s => s;
+        Func<string, string> project = s => s.ToUpper();
+        IEnumerable<string> query = names
+                                .Where(filter)
+                                .OrderBy(extract)
+                                .Select(project);
+        foreach (string item in query)
+
+            Console.WriteLine(item);
+
+        Console.Read();
+    }*/
+
+    private static void UsingAnonymousMethods(string[] names)
+    {
+        Func<string, bool> filter = delegate (string s)
+        {
+            return s.Length == 5;
+        };
+
+        Func<string, string> extract = delegate (string s)
+        {
+            return s;
+        };
+
+        Func<string, string> project = delegate (string s)
+        {
+            return s.ToUpper();
+        };
+        IEnumerable<string> query = names.Where(filter)
+                                          .OrderBy(extract)   
+                                          .Select(project);
+                                    
+        foreach (string item in query)
+
+            Console.WriteLine(item);
+
+    }
+}
