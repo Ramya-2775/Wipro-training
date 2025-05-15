e91b8f1c5756886cdade2638b7f4deef24436a0f
Ramya <ramyarams2775@gmail.com>
2025-05-15 15:50:15 +0530

    employee

---
+++ 15-05-25/MVC/Employee.cs
@@ -0,0 +1,13 @@
+namespace WebApplication4.Models
+{
+    public class Employee
+    {
+            public int id { get; set; }
+            public string Firstname { get; set; }
+            public string Lastname { get; set; }
+            public string Location { get; set; }
+        
+    }
+
+}
+
