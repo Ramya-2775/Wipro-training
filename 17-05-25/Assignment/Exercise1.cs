be75d21b532b9dc4690564b16d5867a5c136fbaf
Ramya <ramyarams2775@gmail.com>
2025-05-17 09:39:38 +0530

    Vehicle maintenance

---
+++ 17-05-25/Assignment/Exercise1.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace VehicleMaintenanceApp
+{
+    interface IMaintenance
+    {
+        void Service();
+        void Repair();
+    }
+    class CarMaintenance : IMaintenance
+    {
+        public void Service()
+        {
+            Console.WriteLine("Car is being serviced");
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine("Car is being repaired");
+        }
+    }
+    class BikeMaintenance : IMaintenance
+    {
+        public void Service()
+        {
+            Console.WriteLine("Bike is being serviced");
+        }
+
+        public void Repair()
+        {
+            Console.WriteLine("Bike is being repaired");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IMaintenance car = new CarMaintenance();
+            IMaintenance bike = new BikeMaintenance();
+
+
+            car.Service();
+            car.Repair();
+
+
+            bike.Service();
+            bike.Repair();
+        }
+    }
+}
