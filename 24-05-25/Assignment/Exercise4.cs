0debc6f6750139dc2fd49fa4cdc67ae044b80590
Ramya <ramyarams2775@gmail.com>
2025-05-24 15:51:29 +0530

    Thread

---
+++ 24-05-25/Assignment/Exercise4.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+/*class Program
+{
+    static async Task DownloadFileAsync(string fileName, int delay)
+    {
+        Console.WriteLine($"{fileName} download started...");
+        await Task.Delay(delay);
+        Console.WriteLine($"{fileName} downloaded.");
+    }
+
+    static async Task Main()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+
+        Task t1 = DownloadFileAsync("File1", 2000);
+        Task t2 = DownloadFileAsync("File2", 3000);
+        Task t3 = DownloadFileAsync("File3", 1000);
+
+        await Task.WhenAll(t1, t2, t3);
+
+        sw.Stop();
+        Console.WriteLine($"All downloads completed in {sw.ElapsedMilliseconds} ms");
+    }
+}*/
---
+++ 24-05-25/Assignment/Exercise6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*class Program
+{
+    static async Task<string> TaskWithDelay(string name, int delay)
+    {
+        await Task.Delay(delay);
+        return name;
+    }
+
+    static async Task Main()
+    {
+        Task<string> t1 = TaskWithDelay("Task1", 3000);
+        Task<string> t2 = TaskWithDelay("Task2", 1000);
+        Task<string> t3 = TaskWithDelay("Task3", 2000);
+
+        Task<string> firstFinished = await Task.WhenAny(t1, t2, t3);
+        Console.WriteLine($"First finished: {firstFinished.Result}");
+    }
+}
+*/
