28de56daba4c00c434405c1e29660b36e54b5fb7
Ramya <ramyarams2775@gmail.com>
2025-05-27 14:20:28 +0530

    Webpage

---
+++ 27-05-25/WebApplication10/Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication10.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection") { }
+        public DbSet<product> products { get; set; }
+    }
+}
---
+++ 27-05-25/WebApplication10/Models/product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication10.Models
+{
+    public class product
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal price { get; set; }
+    }
+}
