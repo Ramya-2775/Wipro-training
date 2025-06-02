49117a338133557fd3d14185ea7aab10bf977ba6
Ramya <ramyarams2775@gmail.com>
2025-06-02 16:53:04 +0530

    student webapi

---
+++ 02-06-25/WebApi/WebApplication15/Controllers/studentsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication15.Data;
+using WebApplication15.Models;
+
+namespace WebApplication15.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class studentsController : ControllerBase
+    {
+        private readonly WebApplication15Context _context;
+
+        public studentsController(WebApplication15Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<student>>> Getstudent()
+        {
+            return await _context.student.ToListAsync();
+        }
+
+        // GET: api/students/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<student>> Getstudent(int id)
+        {
+            var student = await _context.student.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/students/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putstudent(int id, student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!studentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<student>> Poststudent(student student)
+        {
+            _context.student.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getstudent", new { id = student.Id }, student);
+        }
+
+        // DELETE: api/students/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deletestudent(int id)
+        {
+            var student = await _context.student.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.student.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool studentExists(int id)
+        {
+            return _context.student.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ 02-06-25/WebApi/WebApplication15/Models/student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication15.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
