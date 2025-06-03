de83e3f1226ebb6923cb2a15c69045dbb1692eba
Ramya <ramyarams2775@gmail.com>
2025-06-03 10:35:08 +0530

    products webapi

---
+++ 03-06-25/WebApplication17/Controllers/productsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication17.Data;
+using WebApplication17.Models;
+
+namespace WebApplication17.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productsController : ControllerBase
+    {
+        private readonly WebApplication17Context _context;
+
+        public productsController(WebApplication17Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<product>>> Getproduct()
+        {
+            return await _context.product.ToListAsync();
+        }
+
+        // GET: api/products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<product>> Getproduct(int id)
+        {
+            var product = await _context.product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putproduct(int id, product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!productExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<product>> Postproduct(product product)
+        {
+            _context.product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getproduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Deleteproduct(int id)
+        {
+            var product = await _context.product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.product.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool productExists(int id)
+        {
+            return _context.product.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ 03-06-25/WebApplication17/Models/product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication17.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Category { get; set; }
+    }
+}
