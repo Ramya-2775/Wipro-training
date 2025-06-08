7798326ac268296ea72356a5122540ea53637236
Ramya <ramyarams2775@gmail.com>
2025-06-08 21:41:12 +0530

    Project sample

---
+++ 08-06-25/WebApplication19/Models/Feedback.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication19.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Rating  { get; set; }
+        public string Message { get; set; }
+        public DateTime submittedon { get; set; } = DateTime.Now;
+
+    }
+}
