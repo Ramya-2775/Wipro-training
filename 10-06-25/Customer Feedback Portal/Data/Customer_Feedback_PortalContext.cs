7a2ecaf6b8e14e62f420f52f34be852cc53e51ff
Ramya <ramyarams2775@gmail.com>
2025-06-10 11:11:43 +0530

    Project

---
+++ 10-06-25/Customer Feedback Portal/Data/Customer_Feedback_PortalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Customer_Feedback_Portal.Models;
+
+namespace Customer_Feedback_Portal.Data
+{
+    public class Customer_Feedback_PortalContext : DbContext
+    {
+        public Customer_Feedback_PortalContext (DbContextOptions<Customer_Feedback_PortalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer_Feedback_Portal.Models.Feedback> Feedback { get; set; } = default!;
+    }
+}
---
+++ 10-06-25/Customer Feedback Portal/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Customer_Feedback_Portal.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Customer_Feedback_PortalContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Customer_Feedback_PortalContext") ?? throw new InvalidOperationException("Connection string 'Customer_Feedback_PortalContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
